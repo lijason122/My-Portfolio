eb6df5adc5745de2cfab76eb6e4672490a93333a
Jason Li <lijason@sheridancollege.ca>
2025-09-13 16:29:33 -0400

    Add new project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,25 +6,32 @@ import IMG2 from "../../assets/api-web-app.PNG";
 import IMG4 from "../../assets/chatbot-ai.png";
 import IMG5 from "../../assets/e-commerce.PNG";
 import IMG6 from "../../assets/travel-agency.png";
-import IMG7 from "../../assets/github-profile.PNG"
+import IMG7 from "../../assets/ai-teaching-companion.png";
 
 const data = [
     {
-    id: 1,
+      id: 1,
+      image: IMG7,
+      title: "AI Teaching Companion",
+      github: "https://github.com/lijason122/saas-application",
+      demo: "https://ai-teaching-companion.vercel.app",
+    },
+    {
+    id: 2,
     image: IMG6,
     title: "Travel Agency Dashboard",
     github: "https://github.com/lijason122/travel-agency",
     demo: "https://lijason-travel-agency.vercel.app",
   },
   {
-    id: 2,
+    id: 3,
     image: IMG5,
     title: "E-Commerce",
     github: "https://github.com/lijason122/My-ECommerce",
     demo: "https://lijason-ecommerce.netlify.app",
   },
   {
-    id: 3,
+    id: 4,
     image: IMG4,
     title: "AI Chatbot Voice Assistant",
     github:
@@ -32,26 +39,26 @@ const data = [
     demo: "https://chatbot-ai-voice-client.onrender.com/",
   },
   {
-    id: 4,
+    id: 5,
     image: IMG1,
     title: "Bug Tracker",
     github: "https://github.com/lijason122/Bug-Tracker",
     demo: "https://lijason-bug-tracker.netlify.app",
   },
   {
-    id: 5,
+    id: 6,
     image: IMG2,
     title: "Real-Time Weather Web App",
     github: "https://github.com/lijason122/REST-API-Web-Application",
     demo: "https://lijason.pythonanywhere.com",
   },
-  {
-    id: 6,
-    image: IMG7,
-    title: "Check out my github profile!",
-    github: "https://github.com/lijason122",
-    demo: "",
-  },
+  // {
+  //   id: 6,
+  //   image: IMG7,
+  //   title: "Check out my github profile!",
+  //   github: "https://github.com/lijason122",
+  //   demo: "",
+  // },
 ];
 
 const Portfolio = () => {
